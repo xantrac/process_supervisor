2db4ff1b0feebdb1539fff007c52666bc742505c
Marco Dell'Olio <dellolio.marco@gmail.com>
2020-08-07 10:22:19 -0400

    worker logic

--- src/workers.rs
+++ src/workers.rs
@@ -2,34 +2,49 @@ use std::sync::mpsc::channel;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
-use std::fmt;
 
-
-struct Worker {
+pub struct Worker {
     name: String,
     id: i32,
-    sender: Sender<i32>,
-    receiver: Receiver<i32>,
+    sender: Sender<String>,
 }
 
-pub fn start_supervisor(n_workers: i32, event_requester: Sender<String>) -> () {
-    let stuff: Vec<i32> = (1..n_workers)
+pub fn start_supervisor(n_workers: i32, event_requester: Sender<String>) -> Vec<Worker> {
+    let workers: Vec<Worker> = (1..n_workers)
         .map(|n| {
             let cloned_event_requester = Sender::clone(&event_requester);
-            let (consumer_sender, consumer_receiver): (Sender<String>, Receiver<String>) = channel();
-
-            thread::spawn(move || start_worker(n, cloned_event_requester, consumer_receiver));
-            n
+            let (consumer_sender, consumer_receiver): (Sender<String>, Receiver<String>) =
+                channel();
+            thread::spawn(move || {
+                start_worker(
+                    format!("worker#{}", n),
+                    cloned_event_requester,
+                    consumer_receiver,
+                )
+            });
+            Worker {
+                name: format!("worker#{}", n),
+                id: n,
+                sender: consumer_sender,
+            }
         })
         .collect();
-
+    workers
 }
 
-fn start_worker(name: i32, event_requester: Sender<String>, worker_receiver: Receiver<String>) -> () {
+fn start_worker(
+    name: String,
+    event_requester: Sender<String>,
+    worker_receiver: Receiver<String>,
+) -> () {
     loop {
-        let value = format!("Next event for worker {} please", name);
-         event_requester.send(value).unwrap();       
-        thread::sleep(Duration::from_secs(3));
-
+        match worker_receiver.recv() {
+            Ok(value) => println!("{}", value),
+            Err(_) => {
+                let value = format!("Next event for worker {} please", name);
+                event_requester.send(value).unwrap();
+                thread::sleep(Duration::from_secs(3));
+            }
+        }
     }
 }
